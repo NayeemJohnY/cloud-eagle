5115305399436aa47ac5ff8e296dc7791dee6883
NayeemJohnY <njnayeem@gmail.com>
2025-09-17 20:24:56 +0530

    Implement DashboardPageTest to verify managed applications count in dashboard

--- src/test/java/tests/DashboardPageTest.java
+++ src/test/java/tests/DashboardPageTest.java
@@ -1,3 +1,37 @@
 package tests;
 
-public class DashboardPageTest {}
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ApplicationsPage;
+import pages.DashboardPage;
+import pages.LoginPage;
+import pages.SideNavMenu;
+
+public class DashboardPageTest extends BaseTest {
+
+  @Test
+  public void testVerifyManagedApplicationsCountShownInDashboard() {
+    // Step 1: Login
+    String email = readProperties.getProperty("email");
+    String password = readProperties.getProperty("password");
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login(email, password);
+    DashboardPage dashboardPage = new DashboardPage(driver);
+    Assert.assertTrue(dashboardPage.isUserLoggedInToDashboard(email), "User Login Failure");
+
+    // Step 2: Get Application Count in Dashboard
+    int countFromDashboard = dashboardPage.getCountTextOfCard("Managed Applications");
+
+    // Step 3: Navigate to Applications Menu
+    SideNavMenu sideNavMenu = new SideNavMenu(driver);
+    sideNavMenu.navigateToMenuFromSideNav("Applications");
+
+    // Step 4: Get Number of Applications in Applications Page
+    ApplicationsPage applicationsPage = new ApplicationsPage(driver);
+    applicationsPage.waitForApplicationsPageToLoad();
+    int countFromApplicationPage = applicationsPage.getApplicationsRowCount();
+
+    // Step 5: Compare Counts
+    Assert.assertEquals(countFromApplicationPage, countFromDashboard, "Applications Count");
+  }
+}
