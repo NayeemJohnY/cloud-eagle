400ea216aa31d671a76d3029c98b33ae0d9e6d8b
NayeemJohnY <njnayeem@gmail.com>
2025-09-17 20:21:53 +0530

    Refactor DashboardPage class to enhance user login verification and card count retrieval methods

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -8,7 +8,11 @@ public class DashboardPage extends BasePage {
 
   private By dashboardHeader = By.xpath("//h2[text()='Dashboard']");
   private Locator cardTitle = Locator.xpath("//p[contains(@class, 'cardTitle') and text()='%s']");
-  private Locator profileUsername = Locator.xpath("//p[contains(@class, 'userName') and text()='%s']");
+  private Locator profileUsername =
+      Locator.xpath("//p[contains(@class, 'userName') and text()='%s']");
+  private Locator cardCountText =
+      Locator.xpath("//*[contains(@class, 'cardDetail')]" +
+      "[descendant::*[text()='%s']]/descendant::*[contains(@class, 'countText')]");
 
   public DashboardPage(WebDriver driver) {
     super(driver);
@@ -18,5 +22,10 @@ public class DashboardPage extends BasePage {
     webDriverHelper.waitForElementToVisible(cardTitle, "Card Title", "Managed Applications");
     webDriverHelper.waitForElementToVisible(profileUsername, "Profile Username", email);
     return webDriverHelper.isElementVisible(dashboardHeader, "Dashboard Header");
+  }
+
+  public int getCountTextOfCard(String cardTitle) {
+    String countText = webDriverHelper.getElementText(cardCountText, "Card Count Text", "Managed Applications");
+    return Integer.parseInt(countText);
   }
 }
