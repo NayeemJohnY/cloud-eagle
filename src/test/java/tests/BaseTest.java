c7bd9f94a3c71bb71c97c2c6f9be3fb287cfb84c
NayeemJohnY <njnayeem@gmail.com>
2025-09-17 17:47:59 +0530

    Implement BaseTest and LoginPageTest classes with setup and login functionality

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,31 @@
 package tests;
 
+import base.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.ReadProperties;
 
-public class BaseTest {}
+public class BaseTest {
+
+  protected WebDriver driver;
+  protected ReadProperties readProperties = ReadProperties.getInstance();
+ protected static final Logger logger = LogManager.getLogger();
+
+  @BeforeMethod
+  public void setup() {
+    driver = WebDriverManager.getDriver();
+    String url = readProperties.getProperty("url");
+    logger.info("Launching application URL: {}", url);
+    driver.get(url);
+  }
+
+  @AfterMethod
+  public void teardown() {
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+}
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,3 +1,19 @@
 package tests;
 
-public class LoginPageTest {}
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+
+public class LoginPageTest extends BaseTest {
+
+  @Test
+  public void testShouldAbleToLoginWithValidCredentials() {
+    String email = readProperties.getProperty("email");
+    String password = readProperties.getProperty("password");
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login(email, password);
+    DashboardPage dashboardPage = new DashboardPage(driver);
+    Assert.assertTrue(dashboardPage.isUserLoggedInToDashboard(email), "User Login Failure");
+  }
+}
