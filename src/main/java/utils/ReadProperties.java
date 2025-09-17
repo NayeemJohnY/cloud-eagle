9dcf7e47ebc2c3aee489c0c217f8949e493e5828
NayeemJohnY <njnayeem@gmail.com>
2025-09-17 13:11:53 +0530

    Implement ReadProperties class to load environment-specific properties

--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -1,3 +1,33 @@
 package utils;
 
-public class ReadProperties {}
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReadProperties {
+
+  private Properties prop;
+  private static Logger logger = LogManager.getLogger(ReadProperties.class);
+  
+  public ReadProperties() {
+    this.prop = new Properties();
+    String env =
+        System.getenv("env") != null
+            ? System.getenv("env").toLowerCase()
+            : System.getProperty("env", "sandbox").toLowerCase();
+    try (InputStream inputStream =
+        ReadProperties.class.getResourceAsStream(env + "-config.properties")) {
+      if (inputStream == null) {}
+
+      prop.load(inputStream);
+    } catch (IOException e) {
+      logger.error("IOException occurred while reading Properties", e);
+    }
+  }
+
+  public String getProperty(String key) {
+    return prop.getProperty(key).trim();
+  }
+}
